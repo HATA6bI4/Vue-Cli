7f77e08fd4b3cc6028f60bce6e2f3a01e061ea2c
HATA6bI4 <59885730+HATA6bI4@users.noreply.github.com>
2020-11-16 22:35:09 +0600

    1

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="wraper">
+  <div class="wraper-content">
+    <section>
+      <div class="container">
+        <img alt="Vue logo" src="./assets/logo.png">
+      </div>
+    </section>
+  </div>
+</div>
+</template>
+
+<script>
+
+
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
